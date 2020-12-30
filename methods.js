18798a79b5cf43e5b988dd98739c1f9b069885da
soham874 <soham874@gmail.com>
2020-12-30 19:39:04 +0530

    Added : Finally got hover working

--- methods.js
+++ methods.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
-    $(".length_rectangle").click(
-        function() {
-            $(".choose_type").css(
-                "color", "green"
-            );
-        });
+    $(".temperature_rectangle").on({
+
+        mouseenter: function() {
+            $(".temperature_rectangle").toggleClass("temperature_rectangle_active")
+            $(".temperature").toggleClass("temperature_active")
+            $(".temperature_image").toggleClass("temperature_image_active")
+        },
+
+        mouseleave: function() {
+            $(".temperature_rectangle").toggleClass("temperature_rectangle_active")
+            $(".temperature").toggleClass("temperature_active")
+            $(".temperature_image").toggleClass("temperature_image_active")
+        },
+
+    });
 });
