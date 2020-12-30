14a2111e7b131cc2c93903241402796ef1b0d04d
soham874 <soham874@gmail.com>
2020-12-30 20:06:59 +0530

    Added : Finally got click working properly

--- methods.js
+++ methods.js
@@ -1,17 +1,19 @@
+let temp_click = 0
+let length_click = 0
+let volume_click = 0
+
 $(document).ready(function() {
-    $(".temperature_rectangle").on({
+    $(".temperature_rectangle").on(
+        'mouseleave mouseenter click',
+        function(event) {
+            if (temp_click == 0 && event.type != "click") {
+                $(".temperature_rectangle").toggleClass("temperature_rectangle_active")
+                $(".temperature").toggleClass("temperature_active")
+                $(".temperature_image").toggleClass("temperature_image_active")
+            }
 
-        mouseenter: function() {
-            $(".temperature_rectangle").toggleClass("temperature_rectangle_active")
-            $(".temperature").toggleClass("temperature_active")
-            $(".temperature_image").toggleClass("temperature_image_active")
-        },
-
-        mouseleave: function() {
-            $(".temperature_rectangle").toggleClass("temperature_rectangle_active")
-            $(".temperature").toggleClass("temperature_active")
-            $(".temperature_image").toggleClass("temperature_image_active")
-        },
-
-    });
+            if (event.type == "click")
+                temp_click = 1;
+        }
+    );
 });
